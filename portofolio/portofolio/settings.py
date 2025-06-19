9315a1aa903e822cf85a8fb95f433dccf518c988
Zoetatua <32738880+Zoetatua@users.noreply.github.com>
2025-06-19 10:33:09 +0100

    .

--- portofolio/portofolio/settings.py
+++ portofolio/portofolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3jzunvn^=d$y30+l9vp6a_eg(*k!ea#_wz(1@)!1)5!2$=7z4^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'portofolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',  
+        'PASSWORD': 'vyVTNgcqiZGvkscRVxUZvbzyyiWVaKZS',  
+        'HOST': 'shortline.proxy.rlwy.net', 
+        'PORT': '45152',
     }
 }
 
