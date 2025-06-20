c95dd296b69d63849390382b7dc77938e3368804
Zoetatua <32738880+Zoetatua@users.noreply.github.com>
2025-06-20 10:16:34 +0100

    pip update

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,3 @@
-python3 -m pip install -r requirements.txt
-
-python3 manage.py migrate --noinput
-
-python3 manage.py collectstatic --noinput
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
