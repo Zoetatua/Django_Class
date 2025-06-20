aa6b1566d4314387115c1fd742d815ce89164224
Zoetatua <32738880+Zoetatua@users.noreply.github.com>
2025-06-20 09:52:46 +0100

    vercel input

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 python3 -m pip install -r requirements.txt
 
-python3 manage.py migrate
+python3 manage.py migrate --noinput
 
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput
