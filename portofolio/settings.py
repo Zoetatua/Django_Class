e874993b8486d51d0d5c3a6ea079c6144f92aa43
Zoetatua <32738880+Zoetatua@users.noreply.github.com>
2025-10-22 15:08:12 +0100

    Configure static files for Vercel

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -77,14 +77,11 @@ WSGI_APPLICATION = 'portofolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',  
-        'PASSWORD': 'vyVTNgcqiZGvkscRVxUZvbzyyiWVaKZS',  
-        'HOST': 'shortline.proxy.rlwy.net', 
-        'PORT': '45152',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
@@ -122,10 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type    
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
