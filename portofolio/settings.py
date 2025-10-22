c195212c4ab5a0bfa1c2dfb0cd611d48d8e9cefe
Zoetatua <32738880+Zoetatua@users.noreply.github.com>
2025-10-22 15:14:13 +0100

    Update settings.py

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3jzunvn^=d$y30+l9vp6a_eg(*k!ea#_wz(1@)!1)5!2$=7z4^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
